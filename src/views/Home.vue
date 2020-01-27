30021c4f1f69a722e069dd5d7bcdac5d27e18170
Mathias Hansen <me@codemonkey.io>
2020-01-26 22:17:30 -0500

    Basic functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="container mx-auto leading-normal">
-            <h1 class="text-6xl font-bold text-yellow-500 text-center">Here comes the sun</h1>
-            <p class="text-xl text-gray-200 my-2">The new Amazon-Arlington Solar Farm Virginia project is big. 476 acres to be exact. That’s over 360 football fields.</p>
+            <h1 class="text-6xl font-bold text-yellow-500 text-center mb-4">Here comes the sun</h1>
+            <p class="text-xl text-gray-200 my-2">The new Amazon-Arlington Solar Farm Virginia project is big. {{ solarSizeAcres }} acres to be exact. That’s over 360 football fields.</p>
 
             <p class="text-xl text-gray-200 my-2">The solar electricity will power and offset over 80% of the Arlington County government’s electricity use. Our libraries. Stoplights. (Green lights, too.) Fire stations, community centers, the Arlington County Trades Center, the water treatment plant… and more.</p>
 
@@ -14,23 +14,24 @@
 
             <form @submit.prevent="handleFormSubmitted">
                 <input type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded my-8 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="2100 Clarendon Blvd, Arlington, VA 22201" v-model="address" />
+                <div v-if="errorMessage" class="text-red-500 bg-gray-400 font-bold my-4 p-2 rounded">{{ errorMessage }}</div>
             </form>
+
+            <div v-if="matchedBoundary" class="text-gray-200 text-2xl my-16 text-center leading-loose">
+                <p>
+                    You address is in <strong>{{ matchedBoundary.CIVIC }}</strong>
+                    which is <strong>{{ matchedBoundary.ACRES || 'unknown' }} acres.</strong>
+                </p>
+
+                <p v-if="matchedBoundary.ACRES > 0" class="text-4xl">
+                    The Arlington solar farm is {{ Math.round(solarSizeAcres / matchedBoundary.ACRES * 10) / 10 }}x the size of <strong>{{ matchedBoundary.CIVIC }}</strong>.
+                </p>
+            </div>
         </div>
 
     <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true"
              data-projection="EPSG:4326" style="height: 800px">
       <vl-view :zoom.sync="zoom" :center.sync="center"></vl-view>
-
-      <vl-geoloc @update:position="geolocPosition = $event">
-        <template slot-scope="geoloc">
-          <vl-feature v-if="geoloc.position" id="position-feature">
-            <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
-            <vl-style-box>
-              <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-            </vl-style-box>
-          </vl-feature>
-        </template>
-      </vl-geoloc>
 
       <vl-layer-tile id="geocodio" :opacity="0.2">
         <vl-source-xyz url="https://tile-cdn.geocod.io/tiles/geocodio/{z}/{x}/{y}.png"></vl-source-xyz>
@@ -47,12 +48,12 @@
       <vl-layer-vector>
         <vl-feature>
           <vl-geom-point
-            :coordinates="[-77.14578895348512,38.902050311618886]"
+            :coordinates="addressResultPoint || center"
           ></vl-geom-point>
 
           <vl-style-box>
-            <vl-style-circle :radius="40">
-              <vl-style-fill color="yellow"></vl-style-fill>
+            <vl-style-circle :radius="10 * zoom">
+              <vl-style-fill color="rgba(255, 255, 0, 0.5)"></vl-style-fill>
             </vl-style-circle>
           </vl-style-box>
         </vl-feature>
@@ -64,17 +65,24 @@
 <script>
   import axios from 'axios'
   import qs from 'qs'
+  import GeoJsonGeometriesLookup from 'geojson-geometries-lookup';
+  import boundaries from '../boundaries';
+  import acres from '../acres';
+
+  const boundaryLookup = new GeoJsonGeometriesLookup(boundaries);
 
   export default {
     data () {
       return { 
         zoom: 13,
         center: [-77.099113, 38.885484],
-        geolocPosition: undefined,
         address: '',
         addressResult: null,
+        addressResultPoint: null,
         isLoading: false,
-        errorMesasge: null
+        errorMessage: null,
+        matchedBoundary: null,
+        solarSizeAcres: 476
       }
     },
     methods: {
@@ -86,10 +94,27 @@
 
             axios.get('https://api.geocod.io/v1.4/geocode?' + query).then(response => {
                 this.isLoading = false;
-                this.addressResult = response.results[0] || null;
-            }).catch(() => {
+                this.addressResult = response.data.results[0] || null;
+
+                if (this.addressResult) {
+                    this.addressResultPoint = [this.addressResult.location.lng, this.addressResult.location.lat];
+                    this.center = this.addressResultPoint;
+                    this.zoom = 15;
+
+                    const point = {type: "Point", coordinates: this.addressResultPoint};
+                    const match = boundaryLookup.getContainers(point);
+
+                    this.matchedBoundary = match.features.length > 0
+                        ? match.features[0].properties
+                        : null;
+
+                    if (this.matchedBoundary) {
+                        this.matchedBoundary.ACRES = acres[this.matchedBoundary.CIVIC];
+                    }
+                }
+            }).catch(e => {
                 this.isLoading = false;
-                this.errorMessage = 'Could not look up address'
+                this.errorMessage = `Could not look up address: ${e.message || e}`;
             });
         },
     }
